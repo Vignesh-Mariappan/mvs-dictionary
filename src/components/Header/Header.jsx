14874721bd86bfdb772bc8975d0db8d43d3059ae
Vignesh Mariappan <vigneshmv2312@gmail.com>
2022-06-25 19:22:19 +0530

    axios error fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,50 +1,43 @@
 import React from 'react';
 
 import categories from '../../data/categories';
-import { TextField, MenuItem, Select } from '@mui/material';
+import { TextField, MenuItem } from '@mui/material';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import './Header.css';
 
 const Header = ({ category, setCategory, word, setWord, mode }) => {
-    /* darkTheme and ThemeProvider are used to make react material ui components to look better in the darker screen */
-    const darkTheme = createTheme({
-        palette: {
-          mode: mode ==='dark' ? 'dark' : 'light',
-        },
-      });
+  /* darkTheme and ThemeProvider are used to make react material ui components to look better in the darker screen */
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode === 'dark' ? 'dark' : 'light',
+    },
+  });
 
-    const handleSelectChange = language => {
-        // set the category to new language selected
-        setCategory(language);
+  const handleSelectChange = (language) => {
+    // set the category to new language selected
+    setCategory(language);
 
-        // clear the search input field 
-        setWord('')
-    }
+    // clear the search input field
+    setWord('');
+  };
 
-    return (
-        <div className = "header">
-           <span className = "title">MV's Dictionary</span> 
-           <div className = "inputs">
-                <ThemeProvider theme={darkTheme}>
-                    <TextField className = "search" label="Search a word" variant="standard" value = { word }
-                    onChange = { (event) => setWord(event.target.value) } />
-                    <TextField
-                        className = "select"
-                        select
-                        label="Select"
-                        helperText="Please select your language"
-                        value = { category }
-                        onChange = { (event) => handleSelectChange(event.target.value) }
-                        >
-                            {
-                                categories.map(category => <MenuItem key = { category.label } value = { category.label }>{ category.value }</MenuItem>
-                                )
-                            }
-                        </TextField>
-                </ThemeProvider>
-           </div>
-        </div>
-    )
-}
+  return (
+    <div className='header'>
+      <span className='title'>MV's Dictionary</span>
+      <div className='inputs'>
+        <ThemeProvider theme={darkTheme}>
+          <TextField className='search' label='Search a word' variant='standard' value={word} onChange={(event) => setWord(event.target.value)} />
+          <TextField className='select' select label='Select' helperText='Please select your language' value={category} onChange={(event) => handleSelectChange(event.target.value)}>
+            {categories.map((category) => (
+              <MenuItem key={category.label} value={category.label}>
+                {category.value}
+              </MenuItem>
+            ))}
+          </TextField>
+        </ThemeProvider>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
